46b253ee791af9496cfc4615ede64948e49fe915
donatasjan5 <donatasjan5@gmail.com>
2023-09-18 22:00:32 +0300

    Start of the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+
+function Navbar() {
+  return (
+    <>
+    <nav className="navbar">
+    <div className="navbar-conatiner">
+      <Link to="/" className="navbar-logo">
+        MYSTERIOUS
+      </Link>
+    </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
