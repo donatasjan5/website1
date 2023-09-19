fb671d4d6a452a3afe17f1db904f4808c2945b4f
donatasjan5 <donatasjan5@gmail.com>
2023-09-19 20:58:27 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../components/Navbar.css";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
   return (
     <>
-    <nav className="navbar">
-    <div className="navbar-conatiner">
-      <Link to="/" className="navbar-logo">
-        MYSTERIOUS
-      </Link>
-    </div>
-    </nav>
+ <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            <div className="logo-text">MYSTERIOUS</div>
+            <i className="material-symbols-outlined">mystery</i>
+          </Link>
+          <div className="navbar-menu">
+            <div className="material-symbols-outlined">menu</div>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
