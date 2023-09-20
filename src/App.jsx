a25ea58533b43009104999b5aa6f2d9a1da4dc60
donatasjan5 <donatasjan5@gmail.com>
2023-09-20 22:38:40 +0300

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from './components/pages/Home';
+import Home from "./components/pages/Home";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Navbar />
         <Home />
         <Routes>
-          <Route path='/' exact component={Home}/>
+          <Route path="/" exact component={Home} />
         </Routes>
       </Router>
     </>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
 import HeroSect from '../HeroSect';
+import Cards from '../Cards';
 
 function Home () {
     return (
         <>
         <HeroSect />
+        <Cards />
         </>
     )
 }
