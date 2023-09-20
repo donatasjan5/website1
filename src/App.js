4992de5694579b3e1dd74bd04f0afc95a203df99
donatasjan5 <donatasjan5@gmail.com>
2023-09-20 22:05:41 +0300

    Hero

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+        <Home />
         <Routes>
-          <Route path="/" axact />
+          <Route path='/' exact component={Home}/>
         </Routes>
       </Router>
     </>
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+import HeroSect from '../HeroSect';
+
+function Home () {
+    return (
+        <>
+        <HeroSect />
+        </>
+    )
+}
+
+export default Home;
