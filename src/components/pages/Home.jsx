aaec9c548d6720206e22185cd01429d5cb466d4d
donatasjan5 <donatasjan5@gmail.com>
2023-09-24 23:26:54 +0300

    added footer

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../App.css';
 import HeroSect from '../HeroSect';
 import Cards from '../Cards';
+import Footer from '../Footer';
 
 function Home () {
     return (
         <>
         <HeroSect />
         <Cards />
+        <Footer />
         </>
     )
 }
